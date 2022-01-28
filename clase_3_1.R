ffb78caff37151c9846ca378851b4927ed229428
Erwin Saavedra <saavedraerwin@gmail.com>
2022-01-27 21:21:09 -0600

    added non-quantitative vars

--- clase_3_1.R
+++ clase_3_1.R
@@ -14,5 +14,48 @@ df <- bc[, 5:17]
 str(df)
 
 res.pca <- PCA(df, scale.unit = TRUE, graph = FALSE)
+# res.pca <- PCA(df, scale.unit = TRUE, graph = FALSE, ncp = 13)
 
 summary(res.pca)
+
+# obterner los valores propios
+eig.val <- get_eigenvalue(res.pca)
+eig.val
+
+# screen plot
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 50), ncp = 13)
+fviz_screeplot(res.pca)
+
+# obtenemos las variables
+var <- get_pca_var(res.pca)
+var
+var$coord
+var$cos2
+
+library("corrplot")
+
+corrplot(var$cos2, is.corr = FALSE)
+
+fviz_cos2(res.pca, choice = "var", axes = 1:2)
+fviz_pca_var(res.pca, col.var = "cos2")
+
+var$contrib
+
+ind <- get_pca_ind(res.pca)
+ind
+
+ind$coord
+fviz_pca_ind(res.pca, label = "none")
+
+fviz_pca_biplot(res.pca, geom = "point")
+fviz_pca_biplot(res.pca)
+ind
+
+# famd
+res.famd <- FAMD(bc, ncp = 50)
+
+eig.val1 <- get_eigenvalue(res.famd)
+
+str(bc)
+
+fviz_screeplot(res.famd, ncp = 23)
