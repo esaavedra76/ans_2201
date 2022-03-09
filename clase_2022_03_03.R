7ff5f4c63cbc0429123d22365a54b25a8bbdda75
Erwin Saavedra <saavedraerwin@gmail.com>
2022-03-09 00:13:12 -0600

    sync

--- clase_2022_03_03.R
+++ clase_2022_03_03.R
@@ -1,15 +1,17 @@
-library("factoextra")
+library(factoextra)
 library(fpc)
+library(clustertend)
 library(NbClust)
-
-library("readxl")
+library(readxl)
 
 df <- read_excel('D:\\Documents\\DataSpellDirectories\\aprendizaje_no_supervisado_r\\Customer_1.xlsx')
 
 head(df)
 
 set.seed(123)
-hopkins(df,n=nrow(df)-1)
+hopkins(df, n = nrow(df) - 1)
 
-res<-get_clust_tendency(df,n=nrow(df)-1)
+res <- get_clust_tendency(df, n = nrow(df) - 1)
 res$hopkins_stat
+
+fviz_dist(dist(df), show_labels = FALSE) + labs(title = "Datos de Iris")
